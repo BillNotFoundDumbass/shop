7b437d110ff7a759352d2d3affaa2c6888c26f6c
BillNotFoundDumbass <quangminhnguyen0701@gmail.com>
2024-01-25 18:23:21 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var number_cart=0;
+var products=[]
+function add() {
+  
+    
+    document.getElementById("num").innerHTML=++number_cart
+    var name=[]
+   var item = {name:"1",
+              price:"1800$",}
+              products.push(item)
+              console.log(products)
+              
+
+
+
+
+
+
+
+}
+
